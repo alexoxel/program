03e5d2b521316efc161c1b7b2c4bd34b4e75a6a5
alexoxel <143863480+alexoxel@users.noreply.github.com>
2023-09-02 10:50:13 +0500

    tiger

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello")
+import random
+class Tiger:
+    def __int__(self):
+        self.state = "выследить добычу"
+        self.successful_attack = 0.5
+        self.x, self.y = 0, 0
+    def update_state(self, hares):
+        if self.state == "выследить добычу":
+            print("тигр выслеживает добычу")
